edb703ae9d9db9e2246f74c2dfa7ce0ca0f8ee61
nsprng <6e7370726e67@gmail.com>
2019-03-10 16:11:02 +0300

    Added DB check

--- morgue/entrypoint.sh
+++ morgue/entrypoint.sh
@@ -8,13 +8,19 @@ sed -i "s|localhost|$DB_HOST|" /var/www/morgue/config/development.json
 sed -i "s|morgue_password|$MRG_PSWD|" /var/www/morgue/config/development.json
 sed -i "s|Europe/Zurich|$TZ|" /var/www/morgue/config/development.json
 
-mysql -u root -p -h $DB_HOST --password=$DB_PSWD < /var/www/morgue/schemas/create_db.sql
-mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/postmortems.sql
-mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/images.sql
-mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/jira.sql
-mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/links.sql
-mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/irc.sql
-mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/anniversary.sql
-mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/slack.sql
+DB_CHECK=$(mysql -u root -h $DB_HOST --password=$DB_PSWD -s -N -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='morgue'")
+
+if [ -z "$DB_CHECK" ]; then
+    mysql -u root -p -h $DB_HOST --password=$DB_PSWD < /var/www/morgue/schemas/create_db.sql
+    mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/postmortems.sql
+    mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/images.sql
+    mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/jira.sql
+    mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/links.sql
+    mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/irc.sql
+    mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/anniversary.sql
+    mysql -u morgue -p -h $DB_HOST --password=$MRG_PSWD morgue < /var/www/morgue/schemas/slack.sql
+fi
+
+rm -f /run/apache2/apache2.pid
 
 exec "$@"
